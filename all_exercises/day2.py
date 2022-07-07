f2e930806949cfc3a45a3611efae3b4a8879b550
Marcelo-Ferreira-Leite <marcelofleitte@gmail.com>
2022-07-07 20:33:50 -0300

    struggling with day 3(too much drawing)

---
+++ all_exercises/day2.py
@@ -0,0 +1,15 @@
+#Tip Calculator
+
+def tipPercentage(bill_amount, tip_percentage, people):
+    total_bill = bill_amount + (bill_amount * tip_percentage/100)
+    total_each = total_bill / people
+    if people == 1:
+        return f'You will have to pay: ${total_each:.2f}'
+    return f'Each person will pay: ${total_each:.2f}'
+
+print('Welcome to the Tip Calculator')
+bill = float(input('What is the total bill amount? \n$: '))
+tip = float(input('How much tip would you like to give? \nPercent: '))
+people = float(input('How many people to split the bill? \nPeople: '))
+
+print(tipPercentage(bill, tip, people))
